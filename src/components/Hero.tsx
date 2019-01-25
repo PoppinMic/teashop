6579b490c09fa73d84e081c2a7ec51c0ca604869
poppinmic <cjma1990au@gmail.com>
2019-01-26 01:12:27 +1100

    Fix Hero Style

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,16 +3,18 @@ import './Hero.scss';
 
 const Hero = () => 
   <div className="hero">
-    <p className="hero-title">New Arrival</p>
-    <header className="hero-header">
-      <h1>
-        My Tea Shop
-      </h1>
-    </header>
-    <p className="hero-text">
-      My Text from $68
-    </p>
-    <button>my button</button>
+    <div className="hero-container">  
+      <p className="hero-title">New Arrival</p>
+      <header className="hero-header">
+        <h1>
+          My Tea Shop
+        </h1>
+      </header>
+      <p className="hero-text">
+        My Text from $68
+      </p>
+      <button>my button</button>
+    </div>
   </div>
 ;
 
