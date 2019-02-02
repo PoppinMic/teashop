c87b6b086719cf04d1409ad25e4671e30af5b765
Charlie Liu <Chauncely@192-168-1-113.tpgi.com.au>
2019-02-03 02:00:02 +1100

    update rectangle props

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,29 @@ import React, { Component } from 'react';
 
 import Sidebar from './components/Sidebar/';
 import Hero from './components/Hero';
-import RectangleGrid from './components/rectangle/';
+import RectangleGrid, { RectangleItem } from './components/rectangle/';
 import tea1 from './content/tea1.jpg';
 import bg1 from './content/bg1.png';
 
 // import './App.css';
 
 class App extends Component {
+  gridItems: RectangleItem[] = [
+    {
+      text: 'item1',
+      title: 'title1',
+    },
+    {
+      text: 'item2',
+      title: 'title2',
+    }
+  ];
   render() {
     return (
       <div className="App">
         <Sidebar />
         <Hero bgColor="lightblue" bgImage={'url('+bg1+')'}/>
-        <RectangleGrid />
+        <RectangleGrid Items={this.gridItems}></RectangleGrid>
       </div>
     );
   }
--- src/components/rectangle/index.tsx
+++ src/components/rectangle/index.tsx
@@ -3,8 +3,16 @@ import React from 'react';
 import RectangleSmall from './RectangleSmall';
 import './Rectangle.scss';
 
+interface RectangleProps {
+  Items: RectangleItem[];
+}
 
-const RectangleGrid = (props: {title?: string; text?: string}) => 
+export interface RectangleItem {
+  title: string;
+  text: string;
+}
+
+const RectangleGrid = (props: RectangleProps) => 
   <div className="rectangle-grid">
     <RectangleSmall title="Oolong TeaOolong TeaOolong Tea" text="Our finest artisan oolong tea"/>
     <RectangleSmall title="Cinnamon Tea" text="Our finest artisan oolong tea"/>
